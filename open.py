12fb6283f7ee8f2e4fa32857dd5becf6b6151fc9
Matsvei Tsiareshka <matsveitsiareshka@MacBook-Pro-von-Matsvei.local>
2025-04-04 12:57:09 +0200

    Anton tebe podarok

--- open.py
+++ open.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from IPython.display import display, HTML
+import scipy.stats as stats
 
 file_path = "/Users/matsveitsiareshka/Code/hackathon/abfallkalender-wuerzburg.csv"
 
@@ -12,3 +14,24 @@ print(df.head())
 print(df.describe())
 
 print(df.isnull().sum())
+if "Stadtteil" in df.columns:
+    print("\nКоличество уникальных районов (Stadtteil):", df["Stadtteil"].nunique())
+    print("\nСписок районов:")
+    print(df["Stadtteil"].unique())
+
+if "bild" in df.columns and "kategorie" in df.columns:
+    print("")
+    duplicates = df.groupby("bild")["kategorie"].nunique()
+    if (duplicates > 1).any():
+        print("")
+        print(duplicates[duplicates > 1])
+    else:
+        print("")
+
+date_list = df.groupby("Stadtteil")["Datum"].apply(lambda x: sorted(x.unique()))
+if "Stadtteil" in df.columns and "Datum" in df.columns:
+    date_counts = df.groupby("kategorie")[["Stadtteil","Datum"]].nunique()
+
+    dates_multiple_areas = date_counts[date_counts > 1]
+    
+    print(dates_multiple_areas)
